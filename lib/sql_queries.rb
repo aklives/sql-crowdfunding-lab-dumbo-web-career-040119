f44a847bedbdbc72de43013e6131ed095e87f776
Adam Kearney <aklives@hotmail.com>
2019-04-08 20:04:04 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -33,7 +33,12 @@ HAVING success >= 0;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+"SELECT users.name, SUM(pledges.amount) as pledge_amount FROM pledges
+join users
+on users.id = pledges.user_id
+GROUP BY users.name
+ORDER BY pledge_amount"
+
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
